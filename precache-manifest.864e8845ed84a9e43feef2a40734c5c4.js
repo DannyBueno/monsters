08cad06e238ceca20ddc482221916bc25afd129c
DannyBueno <Buenodanny94@gmail.com>
2020-04-21 14:00:52 -0700

    Updates

---
+++ precache-manifest.864e8845ed84a9e43feef2a40734c5c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "743860e071942522ce8bb77e046d9dcc",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "7c46540aecaf5c339625",
+    "url": "/monsters/static/css/main.167e0823.chunk.css"
+  },
+  {
+    "revision": "a1d3efdeec19637be9a2",
+    "url": "/monsters/static/js/2.2651c678.chunk.js"
+  },
+  {
+    "revision": "f7bdcefeba2fb25ff3f9e60ee0681e89",
+    "url": "/monsters/static/js/2.2651c678.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c46540aecaf5c339625",
+    "url": "/monsters/static/js/main.3251daf4.chunk.js"
+  },
+  {
+    "revision": "e20b59329f3960f98de4",
+    "url": "/monsters/static/js/runtime-main.e2917106.js"
+  }
+]);
